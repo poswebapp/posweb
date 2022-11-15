56759d0cc8ef29642ea58e6e686776deed084768
zan <olsencortuna@gmail.com>
2022-11-15 18:54:39 +0800

    minor changes

--- client/src/component/dashboard/Dashboard.js
+++ client/src/component/dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import TopContent from "./TopContent";
 
 const Dashboard = () => {
   return (
-    <div className="bg-white shadow-md rounded-xl grid ml-[20%] mr-auto max-w-[48rem] h-[40rem] p-8 mt-10 border border-zinc-300">
+    <div className="bg-white shadow-md rounded-xl grid ml-[23%] mr-[10%] max-w-[48rem] h-[40rem] p-8 mt-10 border border-zinc-300">
       <TopContent />
     </div>
   );
--- client/src/component/good/Good.js
+++ client/src/component/good/Good.js
@@ -1,9 +1,95 @@
-import React from 'react'
+import React from "react";
 
 const Good = () => {
+  const th = "px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase ";
+ const tr = "px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap" 
   return (
-    <div className='mt-[10%] ml-[28%] mr-auto'>Good</div>
-  )
-}
+    <div className=" bg-white shadow-md rounded-md ml-[23%] flex flex-col mr-auto mt-[15%] p-2">
+      <div className="overflow-x-scroll">
+        <div className="p-1.5 w-full inline-block align-middle">
+          <div className="overflow-x-scroll border rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th scope="col" className={th}>
+                    #
+                  </th>
+                  <th scope="col" className={th}>
+                    ID
+                  </th>
+                  <th scope="col" className={th}>
+                    Name
+                  </th>
+                  <th scope="col" className={th}>
+                    Type
+                  </th>
+                  <th scope="col" className={th}>
+                    Stock
+                  </th>
+                  <th scope="col" className={th}>
+                    Unit
+                  </th>
+                  <th scope="col" className={th}>
+                    Price
+                  </th>
+                  <th scope="col" className={th}>
+                    Action
+                  </th>
+                </tr>
+              </thead>
 
-export default Good
+              <tbody className="divide-y divide-gray-200">
+                <tr>
+                  <td className={tr}> 1 </td>
+                  <td className={tr}> 4e5jsh5dai8aw547bdndn </td>
+                  <td className={tr}> Gas </td>
+                  <td className={tr}> Regular </td>
+                  <td className={tr}> 5 </td>
+                  <td className={tr}> 4 </td>
+                  <td className={tr}> 100 </td>
+                  <td className={tr}> edit, delete </td>
+                </tr>
+    
+                <tr>
+                  <td className={tr}> 1 </td>
+                  <td className={tr}> 4e5jsh5dai8aw547bdndn </td>
+                  <td className={tr}> Gas </td>
+                  <td className={tr}> Regular </td>
+                  <td className={tr}> 5 </td>
+                  <td className={tr}> 4 </td>
+                  <td className={tr}> 100 </td>
+                  <td className={tr}> edit, delete </td>
+                </tr>
+    
+                <tr>
+                  <td className={tr}> 1 </td>
+                  <td className={tr}> 4e5jsh5dai8aw547bdndn </td>
+                  <td className={tr}> Gas </td>
+                  <td className={tr}> Regular </td>
+                  <td className={tr}> 5 </td>
+                  <td className={tr}> 4 </td>
+                  <td className={tr}> 100 </td>
+                  <td className={tr}> edit, delete </td>
+                </tr>
+    
+                <tr>
+                  <td className={tr}> 1 </td>
+                  <td className={tr}> 4e5jsh5dai8aw547bdndn </td>
+                  <td className={tr}> Gas </td>
+                  <td className={tr}> Regular </td>
+                  <td className={tr}> 5 </td>
+                  <td className={tr}> 4 </td>
+                  <td className={tr}> 100 </td>
+                  <td className={tr}> edit, delete </td>
+                </tr>
+    
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Good;
