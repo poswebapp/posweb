98630851d724045e4799b3a6ebc0e8619521f3e2
zan <olsencortuna@gmail.com>
2022-11-19 18:19:53 +0800

    minor changes

--- client/src/component/Form.js
+++ client/src/component/Form.js
@@ -2,14 +2,9 @@ import FormInput from "./input/FormInput";
 import Tag from "./input/Tag";
 import DateInput from "./input/Date";
 import { useEffect, useState } from "react";
-import { useListStore } from "../zustand/listStore";
 import curDate from "./input/curDate";
 
 const Form = ({ id, setId, setShowForm }) => {
-  const createList = useListStore((state) => state.createList);
-  const isLoading = useListStore((state) => state.isLoading);
-  // const updateList = useListStore((state) => state.updateList);
-  const lists = useListStore((state) => state.lists);
 
   const [form, setForm] = useState({
     id: "",
@@ -18,11 +13,11 @@ const Form = ({ id, setId, setShowForm }) => {
     tag: "",
     date: curDate,
   });
-  const list = id ? lists.find((a) => a._id === id) : form;
+  // const list = id ? lists.find((a) => a._id === id) : form;
 
-  useEffect(() => {
-    setForm(list);
-  }, [list]);
+  // useEffect(() => {
+  //   setForm(list);
+  // }, [list]);
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -35,7 +30,7 @@ const Form = ({ id, setId, setShowForm }) => {
     } else {
       if (!id || id === 0) {
         // setForm({ ...form, id: nId });
-        createList(form);
+        // createList(form);
         clear();
       }
       // updateList(form, id);
@@ -51,7 +46,7 @@ const Form = ({ id, setId, setShowForm }) => {
   };
 
   return (
-    <div className="smoke w-full h-full z-80 fixed top-0 left-0 right-0">
+    <div className="bg-zinc-400 w-full h-full z-80 fixed top-0 left-0 right-0">
       <div className="p-3 w-80 bg-white rounded-lg border border-grey-200 flex flex-wrap justify-between align-center gap-2 mt-5 shadow-md mx-auto fixed z-90 bottom-20 right-4">
         <FormInput
           name="name"
@@ -70,18 +65,6 @@ const Form = ({ id, setId, setShowForm }) => {
         <Tag form={form} handleChange={handleChange} />
         <DateInput handleChange={handleChange} date={curDate} />
 
-        <button
-          onClick={handleSubmit}
-          className={` w-min-max px-4 py-2  rounded-full font-semibold h-max text-sm border-2
-          ${
-            isLoading
-              ? "bg-none text-gray-400 border-2 border-grey-400"
-              : "bg-green text-black"
-          }`}
-          disable={isLoading.toString()}
-        >
-          Submit
-        </button>
       </div>
     </div>
   );
--- client/src/component/good/Good.js
+++ client/src/component/good/Good.js
@@ -6,71 +6,71 @@ import { Table } from "./table";
 const Good = () => {
   const tr = "px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap";
   const loading = false;
-  const act = "px-6 py-4 text-sm font-medium text-gray-800 whitespace-wrap flex flex-rows gap-2";
+  const act =
+    "px-6 py-4 text-sm font-medium text-gray-800 whitespace-wrap flex flex-rows gap-2";
+  const list = (
+    <>
+      <tr>
+        <td className={tr}> 1 </td>
+        <td className={tr}> 4e5jsh5dai8aw547bdndn </td>
+        <td className={tr}> Gas </td>
+        <td className={tr}> Regular </td>
+        <td className={tr}> 5 </td>
+        <td className={tr}> 4 </td>
+        <td className={tr}> 100 </td>
+
+        <td className={act}>
+          <BtnEdit loading={loading} />
+          <BtnDelete loading={loading} />
+        </td>
+      </tr>
+
+      <tr>
+        <td className={tr}> 1 </td>
+        <td className={tr}> 4e5jsh5dai8aw547bdndn </td>
+        <td className={tr}> Gas </td>
+        <td className={tr}> Regular </td>
+        <td className={tr}> 5 </td>
+        <td className={tr}> 4 </td>
+        <td className={tr}> 100 </td>
+        <td className={act}>
+          <BtnEdit loading={loading} />
+          <BtnDelete loading={loading} />
+        </td>
+      </tr>
+
+      <tr>
+        <td className={tr}> 1 </td>
+        <td className={tr}> 4e5jsh5dai8aw547bdndn </td>
+        <td className={tr}> Gas </td>
+        <td className={tr}> Regular </td>
+        <td className={tr}> 5 </td>
+        <td className={tr}> 4 </td>
+        <td className={tr}> 100 </td>
+        <td className={act}>
+          <BtnEdit loading={loading} />
+          <BtnDelete loading={loading} />
+        </td>
+      </tr>
+
+      <tr>
+        <td className={tr}> 1 </td>
+        <td className={tr}> 4e5jsh5dai8aw547bdndn </td>
+        <td className={tr}> Gas </td>
+        <td className={tr}> Regular </td>
+        <td className={tr}> 5 </td>
+        <td className={tr}> 4 </td>
+        <td className={tr}> 100 </td>
+        <td className={act}>
+          <BtnEdit loading={loading} />
+          <BtnDelete loading={loading} />
+        </td>
+      </tr>
+    </>
+  );
   return (
     <div>
-      <Table
-        element={
-          <>
-            <tr>
-              <td className={tr}> 1 </td>
-              <td className={tr}> 4e5jsh5dai8aw547bdndn </td>
-              <td className={tr}> Gas </td>
-              <td className={tr}> Regular </td>
-              <td className={tr}> 5 </td>
-              <td className={tr}> 4 </td>
-              <td className={tr}> 100 </td>
-
-              <td className={act}>
-                <BtnEdit loading={loading} />
-                <BtnDelete loading={loading} />
-              </td>
-            </tr>
-
-            <tr>
-              <td className={tr}> 1 </td>
-              <td className={tr}> 4e5jsh5dai8aw547bdndn </td>
-              <td className={tr}> Gas </td>
-              <td className={tr}> Regular </td>
-              <td className={tr}> 5 </td>
-              <td className={tr}> 4 </td>
-              <td className={tr}> 100 </td>
-              <td className={act}>
-                <BtnEdit loading={loading} />
-                <BtnDelete loading={loading} />
-              </td>
-            </tr>
-
-            <tr>
-              <td className={tr}> 1 </td>
-              <td className={tr}> 4e5jsh5dai8aw547bdndn </td>
-              <td className={tr}> Gas </td>
-              <td className={tr}> Regular </td>
-              <td className={tr}> 5 </td>
-              <td className={tr}> 4 </td>
-              <td className={tr}> 100 </td>
-              <td className={act}>
-                <BtnEdit loading={loading} />
-                <BtnDelete loading={loading} />
-              </td>
-            </tr>
-
-            <tr>
-              <td className={tr}> 1 </td>
-              <td className={tr}> 4e5jsh5dai8aw547bdndn </td>
-              <td className={tr}> Gas </td>
-              <td className={tr}> Regular </td>
-              <td className={tr}> 5 </td>
-              <td className={tr}> 4 </td>
-              <td className={tr}> 100 </td>
-              <td className={act}>
-                <BtnEdit loading={loading} />
-                <BtnDelete loading={loading} />
-              </td>
-            </tr>
-          </>
-        }
-      />
+      <Table element={list} />
     </div>
   );
 };
