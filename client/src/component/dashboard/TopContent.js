cabf29f8072e33ad2783ebd3e14f88470b6c2b8b
zan <olsencortuna@gmail.com>
2022-11-23 18:27:54 +0800

    minor chnages

--- client/src/component/dashboard/TopContent.js
+++ client/src/component/dashboard/TopContent.js
@@ -3,7 +3,7 @@ import React from "react";
 const TopContent = () => {
   
   return (
-    <content className="flex flex-row justify-apart content-start grid-cols-4 gap-8 w-full mx-auto">
+    <content className="flex flex-row justify-between content-start grid-cols-4 gap-8 w-full mx-auto h-auto">
       <Content name={"Total Goods"} value={130} border={"border-cyan-600"} />
       <Content name={"Total Suppliers"} value={8} border={"border-amber-600"} />
       <Content name={"Stocks of Goods"} value={130} border={"border-violet-400"} />
@@ -16,7 +16,7 @@ export default TopContent;
 
 export const Content = ({ name, value, border }) => {
   return (
-    <span className={`flex justify-start content-apart  border-l-8 bg-zinc-100 h-30 max-w-40 shadow-md rounded-lg p-4 text-md font-bold text-zinc-600 ${border}`}>
+    <span className={`flex justify-start content-apart  border-l-8 bg-zinc-100 h-24 w-40 shadow-md rounded-lg p-4 text-md font-bold text-zinc-600 ${border}`}>
       <span className="grid gap-2">
         <h4 className="text-zinc-500 font-[500] rale text-sm w-full whitespace-wrap">{name}</h4>
         <p>{value}</p>
---
+++ server/controlers/total.js
@@ -0,0 +1,22 @@
+import Good from '../models/good.js'
+import Supplier from '../models/supplier.js'
+import Outgoing from '../models/outgoing.js'
+import Incoming from '../models/incoming.js'
+
+export const getTotals = async(req,res)=>{
+  try {
+    const good = await Good.countDocuments()
+    const supplier = await Supplier.countDocuments()
+    const incoming = await Incoming.countDocuments()
+    const outgoing = await Outgoing.countDocuments()
+    res.status(200).json({result: {
+      good,
+      supplier,
+      incoming,
+      outgoing
+    }})
+  } catch (err) {
+    console.log(err)
+  }
+}
+
