ed205741eafd363e28f88a312115dd5a3a23fd36
zan <olsencortuna@gmail.com>
2022-11-28 12:28:23 +0800

    minor changes

--- client/src/component/dashboard/Dashboard.js
+++ client/src/component/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import Chart from "./Chart";
 import TopContent from "./TopContent";
 import { totalStore } from "../../zustand/total";
 import Loading from "../Loading";
+import Recent from "./Recent";
 
 const Dashboard = () => {
   const loading = totalStore((state) => state.loading);
@@ -12,9 +13,12 @@ const Dashboard = () => {
       element={
         <div className="flex flex-col justify-center content-start w-full h-full p-4">
           {loading && <Loading />}
-          <div className="mx-auto bg-white shadow-md rounded-xl grid min-w-full  h-[auto p-8 border border-zinc-300 gap-y-10">
+          <div className="mx-auto bg-white shadow-md rounded-xl grid min-w-full  h-[auto p-5 border border-zinc-300 gap-y-10">
             <TopContent />
-            <Chart />
+            <span className="flex w-full justify-between gap-5 flex-row">
+              <Chart />
+              <Recent />
+            </span>
           </div>
           {/* minimum stock  */}
           {/* recent */}
---
+++ client/src/component/dashboard/Recent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Recent = () => {
+  return (
+    <div className="p-4 rounded-lg bg-zinc-100 h-80 w-full shad grid border flex flex-col content-start">
+
+    </div>
+  )
+}
+
+export default Recent
