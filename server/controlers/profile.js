e1becd02f1a8360f1c1a5c6892e59e3cbc42202a
zan <olsencortuna@gmail.com>
2022-12-05 20:08:04 +0800

    sone changes

---
+++ server/controlers/profile.js
@@ -0,0 +1,36 @@
+import User from "../models/user";
+
+
+export const patchEmail = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { email } = req.body;
+    const result = User.findByIdAndUpdate(
+      id,
+      {
+        email,
+      },
+      { new: true }
+    );
+    return res.json(result);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+export const patchContact = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { contact } = req.body;
+    const result = User.findByIdAndUpdate(
+      id,
+      {
+        contact,
+      },
+      { new: true }
+    );
+    return res.json(result);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { patchContact, patchEmail } from '../controlers/profile.js';
 const router = express.Router();
 
 
@@ -7,5 +8,7 @@ import {login,resetPassword, resetPasswordOTP} from '../controlers/user.js';
 router.post('/login', login);
 router.post('/reset', resetPassword);
 router.post('/resetOTP/:id', resetPasswordOTP);
+router.patch('/patchContact/:id', patchContact);
+router.patch('/patchEmail/:id', patchEmail);
 
 export default router;
