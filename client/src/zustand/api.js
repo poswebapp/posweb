47bcf9bd9693566b8e6fcc2c0a895668ea12a453
zan <olsencortuna@gmail.com>
2022-12-05 08:37:58 +0800

    minor chnages

--- client/src/zustand/api.js
+++ client/src/zustand/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: process.env.REACT_APP_API });
 // export const api = axios.create({ baseURL: "http://localhost:8000/" });
+// export const api = axios.create({ baseURL: "https://pos-wbyq.onrender.com/" });
+export const api = axios.create({ baseURL: "https://pos-wbyq.onrender.com" });
