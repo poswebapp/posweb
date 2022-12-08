1962ffceac9743933dc33f5ba3514773aea4f76d
zan <olsencortuna@gmail.com>
2022-12-08 19:03:55 +0800

    minor changes

--- client/src/zustand/api.js
+++ client/src/zustand/api.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: "http://localhost:8000/" });
-// export const api = axios.create({ baseURL: process.env.REACT_APP_API , timeout: 45000});
+// export const api = axios.create({ baseURL: "http://localhost:8000/" });
+export const api = axios.create({ baseURL: process.env.REACT_APP_API , timeout: 45000});
