6e4894d0094bb1a8a6ef2900267e0e383ba1d51e
zan <olsencortuna@gmail.com>
2023-01-28 18:27:15 +0800

    minor changes

--- client/src/component/incoming/Form.js
+++ client/src/component/incoming/Form.js
@@ -6,7 +6,7 @@ import { incomingStore } from "../../zustand/incoming";
 const Form = ({ currentId, data, onChange, onSubmit }) => {
   const loading = incomingStore((state) => state.loading);
   return (
-    <span className=" z-30 fixed top-0 w-screen h-screen left-0 right-0 flex justify-center content-center bg-zinc-800/20 backdrop-blur-sm">
+    <span className=" z-30 fixed top-0 w-[4444px] h-screen left-0 right-0 flex justify-center content-center bg-zinc-800/20 backdrop-blur-sm">
       <form className="w-[20rem] grid gap-2 h-auto p-4 place-content-start bg-white border rounded-lg m-auto shadow-md">
         <h4 className="text-md text-zinc-500 font-bold ml-2">
           {currentId ? "Update Incoming" : "Upload Incoming"}
